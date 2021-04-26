79a939733958c0b35f7bb5249d4c37b8334a33f9
wallravit <wallravit@gmail.com>
2021-04-27 00:51:42 +0700

    format code.

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from api.routes import users
+from api.routes import user_router
 
 
 def create_app() -> FastAPI:
     app = FastAPI(title="card-matching-game")
 
-    app.include_router(users.router)
+    app.include_router(user_router)
     return app
     
