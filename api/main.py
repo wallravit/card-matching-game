1d0f9e228af63e02d2c6eb015048d293570e7d16
wallravit <wallravit@gmail.com>
2021-04-29 21:53:30 +0700

    add game route.

--- api/main.py
+++ api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 
-from api.routes import user_router
-
+from api.routes import user_router, game_router
 
 def create_app() -> FastAPI:
     app = FastAPI(title="card-matching-game")
 
     app.include_router(user_router)
+    app.include_router(game_router)
     return app
