1f8ad9c9899354a6f7eb788f2d692d5aa9ef812a
wallravit <wallravit@gmail.com>
2021-04-29 23:39:22 +0700

    update user repository.

--- api/repositories/user_repository.py
+++ api/repositories/user_repository.py
@@ -1,28 +1,22 @@
-from datetime import datetime
+from logging import Logger
 from typing import Optional
 
 from api.models import UserModel
 from api.schemas import user_schemas
-from api.utils import acquire_logger
 from sqlalchemy.orm import Session
 
 
 class UserRepository:
-    def __init__(self) -> None:
-        self._logger = acquire_logger(self.__class__.__name__)
+    def __init__(self, logger: Logger) -> None:
+        self._logger = self._logger = logger.getChild(self.__class__.__name__)
 
-    def create(self, db: Session, user: user_schemas.UserCreate) -> Optional[UserModel]:
+    def create(self, db: Session, user: UserModel) -> Optional[UserModel]:
         try:
             self._logger.debug(f"create a new user for user {user.username}.")
-            db_user = UserModel(
-                username=user.username,
-                hashed_password=user.password,
-                created_datetime=datetime.now(),
-            )
-            db.add(db_user)
+            db.add(user)
             db.commit()
-            db.refresh(db_user)
-            return db_user
+            db.refresh(user)
+            return user
         except:
             self._logger.exception("create user fail.")
             raise
